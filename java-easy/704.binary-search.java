a28a29558e227a67b53dc2eee3df3f000a740f51
jvxsensei <nkunaf.sf@gmail.com>
2025-09-21 05:29:13 +0200

    use built-in binary search

--- java-easy/704.binary-search.java
+++ java-easy/704.binary-search.java
@@ -5,9 +5,18 @@
  */
 
 // @lc code=start
+
+import java.util.Arrays;
+
 class Solution {
   public int search(int[] nums, int target) {
 
+    int index = Arrays.binarySearch(nums, target);
+    
+    return index < 0 ? -1 : index;
+  }
+
+  int baseBinarySearch(int[] nums, int target) {
     int left = 0;
     int right = nums.length - 1;
 
